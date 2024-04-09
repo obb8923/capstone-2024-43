124660d49d7f13206ad58aa57c9c5077506d73d3
JeongBin Park <obb8923@naver.com>
2024-04-09 15:27:07 +0900

    setting2

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
-    <>
-    </>
+    <BrowserRouter>
+    <Hello/>
+    <Routes>
+      <Route path="/" element={<KKK/>}/>
+      <Route path="/KK/:kk" element={<KK/>}/>
+      <Route path="*" element={<EmptyPage />}></Route>
+
+    </Routes>
+    <Footer/>
+   </BrowserRouter>
   );
 }
 
