acb03f76c91ef3dd2c24de9a9e271f8de1436ecf
hyeyeong-nam <85778340+hyeyeong-nam@users.noreply.github.com>
2024-04-12 17:21:29 +0900

    mainpage

--- front/src/components/ToolBar.js
+++ front/src/components/ToolBar.js
@@ -1,8 +1,14 @@
 import styles from '../css/ToolBar.module.css';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 function ToolBar(){
 
     return (<>
     <div className={styles.toolbarBody}>
+        <Link to="/WritePage">
+            <button>글쓰기</button>
+        </Link>
 
     </div>
     </>);
---
+++ front/src/components/WritePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function WritePage() {
+  return (
+    <div>
+      <h1>글쓰기 페이지</h1>
+      <button>저장</button>
+      {/* 글쓰기 페이지에 해당하는 컴포넌트 */}
+    </div>
+  );
+}
+
+export default WritePage;
