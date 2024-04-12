bfbcccac1deaf61d8469f0af3f5ed4f3df5a71bc
JeongBin Park <obb8923@naver.com>
2024-04-12 23:36:29 +0900

    NavButton.js

--- front/src/components/NavButton.js
+++ front/src/components/NavButton.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function NavButton(props){
     const nav = props.nav;
  return (<>
-    <div>
+    <div className={styles.button}>
     <Link className={styles.a} to={`/${nav}`}>{nav}</Link>
     </div>
  </>);   
