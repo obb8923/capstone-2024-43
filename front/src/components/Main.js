2e19cd9d0bead2455fafc7139f9f988821cff53d
JeongBin Park <obb8923@naver.com>
2024-04-15 18:25:16 +0900

    infiniteScroll

--- front/src/components/Main.js
+++ front/src/components/Main.js
@@ -1,7 +1,10 @@
+import ScrollView from "./ScrollView";
+
 function Main(){// 홈페이지
 
     return (<>
         <h1>hi, main</h1>
+        <ScrollView style={{ margin: 'auto,0', width: '40%' }} />
     </>);
 }
 
