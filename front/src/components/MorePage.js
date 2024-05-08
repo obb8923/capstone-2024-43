e202d4fc8e488e461cc8b11689aa8c8a6ff8617e
JeongBin Park <obb8923@naver.com>
2024-05-08 11:12:53 +0900

    morePage

--- front/src/components/MorePage.js
+++ front/src/components/MorePage.js
@@ -1,31 +1,56 @@
-import React from "react";
-import styles from "../css/MorePage.module.css"
-function  MorePage(){// 더보기 페이지
-//로그아웃, 정보수정, 공지사항, 문의하기, 
-//쓴글
-//임시저장글
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import styles from "../css/MorePage.module.css";
 
-    
+function MorePage() {
+    const UID = useSelector(state => state.UID);
+    const [posts, setPosts] = useState([]); 
 
- return(<>
-    <div className={styles.box}>
-        {/*기능 버튼들 */}
-        <div className={styles.buttonBox}>
-        <div></div><div></div><div></div><div></div>
-        </div>
-        {/* 쓴 글 */}
-        <div className={styles.libraryBox}>
-            <h2>내 서재</h2>
-            <div></div><div></div><div></div><div></div><div></div>
-            <div></div><div></div><div></div><div></div><div></div>
-            <div></div><div></div><div></div><div></div><div></div>
-        </div>
-        {/* 임시저장 */}
-        <div className={styles.libraryBox}>
+    useEffect(() => {
+        if (UID !== '') {
+            fetch("/api/library", {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({UID: UID})
+            })
+            .then(res => res.json())
+            .then(data => {
+                setPosts(data.result); 
+            })
+            .catch(error => {
+                console.error("Error:", error);
+            });
+        }
+    }, []);
 
-        </div>
-    </div>
- </>);
+    return (
+        <>
+            <div className={styles.box}>
+                {/* Functional buttons */}
+                <div className={styles.buttonBox}>
+                    {/* 4 elements will be included like this */}
+                    <div></div><div></div><div></div><div></div>
+                </div>
+                {/* Written Posts */}
+                <div className={styles.libraryBox}>
+                    <h2>내 서재</h2>
+                    <div className={styles.postsContainer}>
+                        {posts.map(post => (
+                            <div key={post.postID} className={styles.postBox}>
+                                {post.title}  {/* Assuming posts have a title property */}
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                {/* Drafts */}
+                <div className={styles.libraryBox}>
+                    {/* Other components for drafts could be similar */}
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default MorePage;
