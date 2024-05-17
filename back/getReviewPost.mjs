50a13241eaa091921bb03165b0809cc138986efa
Hyeonseok Yoo <evermongwan@gmail.com>
2024-05-17 15:55:08 +0900

    update crawler APIS
    1. Make test query of ISBN in books table
    2. Move prettier from object prototype to local function
    3. Update actions of crawler

--- back/getReviewPost.mjs
+++ back/getReviewPost.mjs
@@ -1,8 +1,8 @@
 import { Builder, Browser, By, until, Key } from 'selenium-webdriver';
-import fs from 'fs';
 import mysql from 'mysql2';
 import db_config from './db-config.json' assert { type: "json" };
 import { requestAladin } from './bookAPI/aladin.js';
+import { createHash } from 'crypto';
 
 const sleep = ms => new Promise( resolve => setTimeout(resolve, ms) );
 const connection = mysql.createConnection({
@@ -13,23 +13,23 @@ const connection = mysql.createConnection({
 });
 connection.connect();
 
-const uid = "8O6VMedKBRUI7VLsmmC6NpAWbYI3";
-const today = new Date().toISOString().slice(0, 10);
-
-export const saveReviewPost = async () => {
+export const saveReviewPost = async (book) => {
     const driver = await new Builder().forBrowser(Browser.CHROME).build();
-    // console.log(book.link);
-    await driver.get(`https://www.aladin.co.kr/shop/wproduct.aspx?ItemId=${itemId}`);
-    // await driver.get(book.link);
+    await driver.get(book.link);
     
     let hundred_list = [];
     let my_review_list = [];
 
     try {
-        await driver.executeScript("window.scrollTo(0, document.body.scrollHeight)");
-        await driver.wait(until.elementLocated(By.id("CommentReviewList"), 500));
+        await driver.wait(until.elementLocated(By.className("Ere_bo_title")), 5000);
+        for (let i = 0; i < 50; i++) {
+            await driver.executeScript(
+                "window.scrollBy(0, window.innerHeight)");
+            await sleep(100);
+        }
+        await driver.wait(until.elementLocated(By.id("CommentReviewList"), 5000));
         
-        let tabTotal = await driver.wait(until.elementLocated(By.id("tabTotal")), 500);
+        let tabTotal = await driver.wait(until.elementLocated(By.id("tabTotal")), 5000);
         await tabTotal.sendKeys(Key.ENTER);
         console.log(">> 100자평 전체 목록 전환");
 
@@ -40,7 +40,9 @@ export const saveReviewPost = async () => {
 
         for await (let element of await driver.findElements(By.xpath(
             `//div[contains(@id, "div_commentReviewPaper")]/div/a[1]`))) {
-            hundred_list.push(await element.getText());
+            let reviewText = await element.getText();
+            if (reviewText.length > 0)
+                hundred_list.push(reviewText);
         }
         console.log(">> 100자평 리스트 추가");
 
@@ -57,7 +59,9 @@ export const saveReviewPost = async () => {
 
         for await (let element of await driver.findElements(By.xpath(
             `//div[@class='paper-contents']/a`))) {
-            my_review_list.push(await element.getAttribute("innerHTML"));
+            let reviewText = await element.getAttribute("innerHTML");
+            if (reviewText.length > 0)
+                my_review_list.push(reviewText);
         }
         console.log(">> 마이리뷰 리스트 추가");
 
@@ -69,24 +73,46 @@ export const saveReviewPost = async () => {
         console.log(e);
     } finally {
         await driver.quit();
-        
-        hundred_list.forEach(review => {
-            connection.query(
-                `INSERT IGNORE INTO posts (postID, body, UID, status, create_at, isbn, title)
-                VALUES ("${uid+today}", "${review}", "${uid}", "good", "${today}", "${book.isbn}", "", )`)
-        });
-        
-        my_review_list.forEach(review => {
-            connection.query(
-                `INSERT IGNORE INTO posts (postID, body, UID, status, create_at, isbn, title)
-                VALUES ("${uid+today}", "${review}", "${uid}", "good", "${today}", "${book.isbn}", "", )`)
-        });
+    }
 
-    }    
+    const uid = "8O6VMedKBRUI7VLsmmC6NpAWbYI3";
+    const today = new Date().toISOString().slice(0, 10);
+    
+    const getTimestamp = () => Math.floor(Date.now() / 1000).toString();
+    const getHash = (str) => 
+        createHash('sha256').update(str+getTimestamp()).digest('hex').slice(0, 42);
+
+    hundred_list.forEach(review => {
+        connection.query(
+            "INSERT IGNORE INTO posts (postID, body, UID, status, create_at, isbn, title) VALUE (?, ?, ?, ?, ?, ?, ?);",
+            [getHash(uid+review), review, uid, "good", today, book.isbn, ""]
+        );
+    });
+    
+    my_review_list.forEach(review => {
+        connection.query(
+            "INSERT IGNORE INTO posts (postID, body, UID, status, create_at, isbn, title) VALUE (?, ?, ?, ?, ?, ?, ?);",
+            [getHash(uid+review), review, uid, "good", today, book.isbn, ""]
+        );
+    });
+
+    return;
 }
 
+// requestAladin('lookUp', '9788954442718').then(async res => {
+//     let book = res.item;
+//     await saveReviewPost(book);
+//     process.exit();
+// });
 
-requestAladin('lookUp', '9788954442718').then(res => {
-    let book = res.item;
-    saveReviewPost(book);
+connection.query("SELECT isbn FROM books", async (queryError, queryResponse) => {
+    for (let b of queryResponse) {
+        if (b.isbn.length < 13) continue;
+        await requestAladin('lookUp', b.isbn).then(async res => {
+            let book = res.item;
+            await saveReviewPost(book);
+        })
+    }
+
+    connection.destroy();
 });
